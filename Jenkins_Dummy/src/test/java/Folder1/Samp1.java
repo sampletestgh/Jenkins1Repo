176b4ce434ca453334eb823655efff8add45a236
sampletestgh <lakshmi.vinu19@gmail.com>
2023-07-12 10:58:39 +0530

    committeddd

---
+++ Jenkins_Dummy/src/test/java/Folder1/Samp1.java
@@ -0,0 +1,14 @@
+package Folder1;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Samp1 {
+
+	public void m1()
+	{
+		System.out.println("Jenkins from GitBaaaaaaaaashhh...");
+
+	}
+
+}
